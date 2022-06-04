8162acb854d0667bcce774ffee91365143805638
gochelias <56095957+gochelias@users.noreply.github.com>
2022-06-03 22:51:08 -0500

    feat(web): customized scroll bar for task comments

--- apps/web/src/components/task/TaskComments.tsx
+++ apps/web/src/components/task/TaskComments.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Fragment } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 export const TaskComments = () => {
 	const comments: any[] = [
@@ -77,42 +78,56 @@ export const TaskComments = () => {
 	return (
 		<>
 			<div className="h-full pb-20 pt-60">
-				<div className="h-full overflow-y-auto">
-					{comments.length > 0 ? (
-						comments.map((comment: any) => (
-							<Fragment>
-								<div className="mb-8 flex space-x-4 rounded-2xl">
-									<div className="relative h-10 w-10 overflow-hidden rounded-full">
-										<Image
-											alt="avatar"
-											layout="fill"
-											src={comment.author.avatar}
-										/>
-									</div>
-									<div className="space-y-2">
-										<div className="">
-											<p className="space-x-2 font-semibold leading-none">
-												{`${comment.author.name}`}
-												<span className="text-100-light leading- text-base font-normal">
-													{` · ${comment.createdAt}`}
-												</span>
-											</p>
-											<p className="text-100-light text-base leading-none">
-												{`${comment.author.username}`}
-											</p>
-										</div>
-										<p className="flex w-full max-w-[626.017px] flex-row rounded-xl rounded-tl-none bg-slate-200 px-3 py-2 text-base">{`${comment.content}`}</p>
-									</div>
-								</div>
-							</Fragment>
-						))
-					) : (
-						<div>No Comments</div>
+				<Scrollbars
+					thumbSize={100}
+					renderThumbVertical={({ style, ...props }) => (
+						<div
+							{...props}
+							style={{
+								...style,
+								backgroundColor: '#94A3B8',
+								borderRadius: '100px',
+							}}
+						/>
 					)}
-				</div>
+				>
+					<div className="h-full">
+						{comments.length > 0 ? (
+							comments.map((comment: any) => (
+								<Fragment>
+									<div className="mb-8 flex space-x-4 rounded-2xl">
+										<div className="relative h-10 w-10 overflow-hidden rounded-full">
+											<Image
+												alt="avatar"
+												layout="fill"
+												src={comment.author.avatar}
+											/>
+										</div>
+										<div className="space-y-2">
+											<div className="">
+												<p className="space-x-2 font-semibold leading-none">
+													{`${comment.author.name}`}
+													<span className="text-100-light leading- text-base font-normal">
+														{` · ${comment.createdAt}`}
+													</span>
+												</p>
+												<p className="text-100-light text-base leading-none">
+													{`${comment.author.username}`}
+												</p>
+											</div>
+											<p className="flex w-full max-w-[626.017px] flex-row rounded-xl rounded-tl-none bg-slate-200 px-3 py-2 text-base">{`${comment.content}`}</p>
+										</div>
+									</div>
+								</Fragment>
+							))
+						) : (
+							<div>No Comments</div>
+						)}
+					</div>
+				</Scrollbars>
 				<div className="flex h-20 items-end pb-1">
 					<input
-						className="bg-300-light focus:ring-offset-blue-light focus:ring-blue-light/50 placeholder:text-100-light w-full self-end rounded-2xl px-4 py-3 transition-transform duration-300 ease-out focus:ring-2 focus:ring-offset-2"
+						className="bg-300-light focus:ring-offset-blue-light focus:ring-blue-light/50 placeholder:text-100-light w-full self-end rounded-xl px-4 py-3 transition-transform duration-300 ease-out focus:ring-2 focus:ring-offset-2"
 						type="text"
 						name=""
 						id=""
