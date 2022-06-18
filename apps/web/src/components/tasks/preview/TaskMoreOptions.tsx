d4974038fbdb03f6ed1612991b4ce893b5d3faec
gochelias <56095957+gochelias@users.noreply.github.com>
2022-06-18 11:40:01 -0500

    fix(web): delete button in red

--- apps/web/src/components/tasks/preview/TaskMoreOptions.tsx
+++ apps/web/src/components/tasks/preview/TaskMoreOptions.tsx
@@ -10,7 +10,7 @@ export const TaskMoreOptions = ({ setIsOpen }: any) => {
 				<Menu.Button className="icon-button icon-button-hover">
 					<MoreIcon className="rotate-90" />
 				</Menu.Button>
-				<Menu.Items className="dark:text-100-dark absolute right-0 mt-4 w-64 space-y-2 rounded-3xl bg-white p-2 text-slate-500 shadow-lg dark:bg-slate-800">
+				<Menu.Items className="absolute right-0 mt-4 w-64 space-y-2 rounded-3xl bg-white p-2 text-slate-500 shadow-lg dark:bg-slate-800 dark:text-slate-400">
 					<Menu.Item>
 						{({ active }: any) => (
 							<button
@@ -37,16 +37,10 @@ export const TaskMoreOptions = ({ setIsOpen }: any) => {
 						)}
 					</Menu.Item>
 					<Menu.Item>
-						{({ active }: any) => (
-							<button
-								className={`${
-									active && 'bg-red-500 text-white'
-								} flex w-full items-center space-x-2 rounded-2xl px-4 py-3`}
-							>
-								<TrashIcon className="mr-3" />
-								Delete
-							</button>
-						)}
+						<button className="flex w-full items-center space-x-2 rounded-2xl px-4 py-3 text-red-500 hover:bg-red-500 hover:text-white">
+							<TrashIcon className="mr-3" />
+							Delete
+						</button>
 					</Menu.Item>
 				</Menu.Items>
 			</Menu>
