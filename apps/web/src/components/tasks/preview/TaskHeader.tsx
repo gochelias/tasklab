2a3960a841b3ec5b1c97a54d3dea147e6418dfea
gochelias <56095957+gochelias@users.noreply.github.com>
2022-06-19 18:25:52 -0500

    fix(web): date decoration color

--- apps/web/src/components/tasks/preview/TaskHeader.tsx
+++ apps/web/src/components/tasks/preview/TaskHeader.tsx
@@ -31,14 +31,14 @@ export const TaskHeader = () => {
 						Started{' '}
 						<span
 							title={startsAtDate}
-							className="dark:decoration-200-dark text-slate-700 underline decoration-slate-300 dark:text-slate-200"
+							className="text-slate-700 underline decoration-slate-300 dark:text-slate-200 dark:decoration-slate-600"
 						>
 							{startsAt}
 						</span>{' '}
 						and ends{' '}
 						<span
 							title={endsAtDate}
-							className="dark:decoration-200-dark text-slate-700 underline decoration-slate-300 dark:text-slate-200"
+							className="text-slate-700 underline decoration-slate-300 dark:text-slate-200 dark:decoration-slate-600"
 						>{`${endsAt}`}</span>
 					</p>
 				</div>
