c9612af1459931ffe4c4d7ba1c16adff8940ab33
gochelias <56095957+gochelias@users.noreply.github.com>
2022-07-01 20:59:33 -0500

    fix(web): in the command palette changed the radius border, opacity and background color

--- apps/web/src/components/CommandPalette.tsx
+++ apps/web/src/components/CommandPalette.tsx
@@ -17,7 +17,7 @@ export const CommandPalette = ({ isOpen, setIsOpen }: any) => (
 					leaveFrom="opacity-100"
 					leaveTo="opacity-0"
 				>
-					<Dialog.Overlay className="bg-slate-dark fixed inset-0 bg-opacity-30 filter dark:bg-opacity-60" />
+					<Dialog.Overlay className="bg-slate-dark fixed inset-0 bg-opacity-40 filter dark:bg-[#000] dark:bg-opacity-75" />
 				</Transition.Child>
 				<Transition.Child
 					as={Fragment}
@@ -28,13 +28,13 @@ export const CommandPalette = ({ isOpen, setIsOpen }: any) => (
 					leaveFrom="opacity-100"
 					leaveTo="opacity-0"
 				>
-					<div className="dark:shadow-slate-dark relative mx-auto max-w-4xl rounded-3xl border-slate-700 bg-white p-2 shadow-xl shadow-slate-600/25  dark:bg-slate-700 dark:shadow-lg">
+					<div className="dark:shadow-slate-dark relative mx-auto max-w-4xl overflow-hidden rounded-2xl border-slate-700 bg-white shadow-xl shadow-slate-600/25 outline outline-1 outline-slate-100 dark:bg-slate-900 dark:shadow-lg dark:outline-slate-700">
 						<input
 							type="text"
 							name=""
 							id=""
 							placeholder="Search"
-							className="h-14 w-full rounded-2xl bg-transparent px-4 text-xl text-slate-700 placeholder:text-slate-400 dark:text-slate-200 dark:placeholder:text-slate-500"
+							className="w-full rounded-2xl bg-transparent px-6 py-4 text-xl text-slate-600 placeholder:text-slate-400 dark:text-slate-300 dark:placeholder:text-slate-500"
 						/>
 					</div>
 				</Transition.Child>
