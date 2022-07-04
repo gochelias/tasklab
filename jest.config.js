e42c8005f32562c9a93cdf8e9b061be5e103e010
gochelias <56095957+gochelias@users.noreply.github.com>
2022-07-03 21:04:05 -0500

    chore(jest): comments removed

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,6 @@
 
 const config = {
 	roots: ['apps/server'],
-	// collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts,tsx}'],
-	// moduleFileExtensions: ['js', 'json', 'ts'],
-	// testRegex: '.*\\.spec\\.ts$',
 	transform: {
 		'^.+\\.(t|j)s$': 'ts-jest',
 	},
