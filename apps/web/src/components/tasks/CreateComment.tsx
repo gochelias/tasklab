7a34517159ec02e52c100f63a494f2230a2149f2
gochelias <56095957+gochelias@users.noreply.github.com>
2022-07-13 20:59:21 -0500

    fix(web): the background color of the input to create a comment has been fixed

--- apps/web/src/components/tasks/CreateComment.tsx
+++ apps/web/src/components/tasks/CreateComment.tsx
@@ -8,8 +8,8 @@ export const CreateComment = () => {
 	return (
 		<>
 			<Formik initialValues={{ comment: '' }} onSubmit={handlerOnSubmit}>
-				<Form className="flex w-full overflow-hidden rounded-3xl bg-slate-100 p-2 dark:bg-slate-700">
-					<div className="flex-auto">
+				<Form className="flex w-full space-x-2 overflow-hidden rounded-3xl border border-slate-200 p-2 dark:border-slate-700">
+					<div className="flex-auto rounded-2xl bg-slate-100 dark:bg-slate-700">
 						<Field
 							className="flex h-12 w-full bg-transparent px-4 text-lg text-slate-700 placeholder:text-slate-400 dark:text-slate-200 dark:placeholder:text-slate-500"
 							id="comment"
@@ -20,7 +20,7 @@ export const CreateComment = () => {
 					</div>
 					<button
 						type="submit"
-						className="flex h-12 w-12 items-center justify-center rounded-2xl bg-slate-300 text-slate-600 dark:bg-slate-800 dark:text-slate-200"
+						className="flex h-12 w-12 items-center justify-center rounded-2xl bg-slate-200 text-slate-600 dark:bg-slate-700 dark:text-slate-200"
 					>
 						<SendIcon active={true} />
 					</button>
