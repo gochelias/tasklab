6b6d5f705e39fdd37de230a3897f5b9305262afc
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-02 18:11:03 -0500

    feat(web): added a line outside of input to create comments when focused

--- apps/web/src/components/tasks/CreateComment.tsx
+++ apps/web/src/components/tasks/CreateComment.tsx
@@ -8,16 +8,14 @@ export const CreateComment = () => {
 	return (
 		<>
 			<Formik initialValues={{ comment: '' }} onSubmit={handlerOnSubmit}>
-				<Form className="flex w-full space-x-2">
-					<div className="w-full rounded-2xl bg-slate-100 dark:bg-slate-700">
-						<Field
-							className="flex h-12 w-full bg-transparent p-4 text-lg text-slate-700 placeholder:text-slate-400 dark:text-slate-200 dark:placeholder:text-slate-500"
-							id="comment"
-							name="comment"
-							type="text"
-							placeholder="Add a comment..."
-						/>
-					</div>
+				<Form className="flex w-full space-x-4">
+					<Field
+						className="flex h-12 w-full rounded-2xl bg-slate-100 bg-transparent p-4 text-lg text-slate-700 ring-blue-500/40 placeholder:text-slate-400 focus:outline focus:outline-1 focus:outline-blue-500 focus:ring-4 dark:bg-slate-700 dark:text-slate-200 dark:placeholder:text-slate-500"
+						id="comment"
+						name="comment"
+						type="text"
+						placeholder="Add a comment..."
+					/>
 					<button
 						type="submit"
 						className="flex h-12 w-12 items-center justify-center rounded-2xl bg-slate-200 text-slate-600 dark:bg-slate-700 dark:text-slate-200"
