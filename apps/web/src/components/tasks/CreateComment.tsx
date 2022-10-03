8844b9ead5ad31c120d9ca0fc3e1185e334d8351
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-03 12:08:32 -0500

    feat(web): the style of the input to create comments has been changed

--- apps/web/src/components/tasks/CreateComment.tsx
+++ apps/web/src/components/tasks/CreateComment.tsx
@@ -11,7 +11,7 @@ export const CreateComment = () => {
 			<Formik initialValues={{ comment: '' }} onSubmit={handlerOnSubmit}>
 				<Form className="flex w-full space-x-4">
 					<Field
-						className="flex h-12 flex-auto rounded-2xl bg-slate-100 bg-transparent p-4 text-lg text-slate-700 ring-blue-500/40 placeholder:text-slate-400 focus:outline focus:outline-1 focus:outline-blue-500 focus:ring-4 dark:bg-slate-700 dark:text-slate-200 dark:placeholder:text-slate-500"
+						className="flex h-12 flex-auto rounded-2xl bg-slate-100 bg-transparent p-4 text-lg text-slate-700 ring-blue-500 placeholder:text-slate-400 focus:ring-4 dark:bg-slate-700 dark:text-slate-200 dark:placeholder:text-slate-500"
 						id="comment"
 						name="comment"
 						type="text"
